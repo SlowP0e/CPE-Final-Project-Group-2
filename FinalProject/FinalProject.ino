5fe61acdba2f593202317868bad4ba3422c3091e
rileyp97 <rileypadilla95@gmail.com>
2025-12-13 22:28:45 -0800

    Updated Final Code with a few bug fixes
    Updated Final Code with a few bug fixes.

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -14,23 +14,24 @@
 #include <DHT.h>
 
 // Pin Definitions
-#define PIN_LED_BLUE      "PH4"   // PH4 D7. OUT
-#define PIN_LED_GREEN     "PH5"   // PH5 D8. OUT
-#define PIN_LED_YELLOW    "PH6"   // PH6 D9. OUT
+#define PIN_LED_BLUE      "PH4"   // PH4 D7  OUT
+#define PIN_LED_GREEN     "PH5"   // PH5 D8  OUT
+#define PIN_LED_YELLOW    "PH6"   // PH6 D9  OUT
 #define PIN_LED_RED       "PB4"   // PB4 D10 OUT
 
 #define PIN_BTN_START     "PE4"   // PE4 D2  IN
+#define PIN_BTN_START_ISR 2       // D2, to be used with interrupt
 #define PIN_BTN_STOP      "PE5"   // PE5 D3  IN
-#define PIN_BTN_CLEAR     "PG5"   // PG5 D4  IN
+#define PIN_BTN_RESET     "PG5"   // PG5 D4  IN
 
 #define PIN_FAN           "PB6"   // PB6 D12
-#define PIN_DHT11         22      // PA0 D22, to be used with DHT library 
+#define PIN_DHT11         22      // PA0 D22, to be used with DHT library
 #define PIN_WATER_SENSOR  0       // Analog 0, to be used with adcRead()
 #define PIN_POTENTIOMETER 1       // Analog 1, to be used with adcRead()
 
 // Pin definitions for clock
-#define PIN_CLK_SDA 20      // 
-#define PIN_CLK_SCL 21      // 
+#define PIN_CLK_SDA 20      //
+#define PIN_CLK_SCL 21      //
 
 // Pin definitions for LCD
 #define PIN_DIS_RS 30       // D30
@@ -39,7 +40,7 @@
 #define PIN_DIS_D5 33       // D33
 #define PIN_DIS_D6 34       // D34
 #define PIN_DIS_D7 35       // D35
-#define LCD_UPDATE_INTERVAL 5000UL  // 5 seconds (was 60000)
+#define LCD_UPDATE_INTERVAL 3000UL  // 1 minute
 
 // Pin definitions for stepper motor library
 #define STEPPER_STEPS 1024
@@ -54,46 +55,18 @@
 
 #define DHTTYPE DHT11
 
-
 // UART Pointers
-volatile unsigned char* pUCSR0A = (unsigned char*) 0x00C0;
-volatile unsigned char* pUCSR0B = (unsigned char*) 0x00C1;
-volatile unsigned char* pUCSR0C = (unsigned char*) 0x00C2;
-volatile unsigned int*  pUBRR0  = (unsigned  int*) 0x00C4;
-volatile unsigned char* pUDR0   = (unsigned char*) 0x00C6;
+volatile unsigned char* _UCSR0A = (unsigned char*) 0x00C0;
+volatile unsigned char* _UCSR0B = (unsigned char*) 0x00C1;
+volatile unsigned char* _UCSR0C = (unsigned char*) 0x00C2;
+volatile unsigned int*  _UBRR0  = (unsigned  int*) 0x00C4;
+volatile unsigned char* _UDR0   = (unsigned char*) 0x00C6;
 
 // ADC Pointers
-volatile unsigned char* pADMUX    = (unsigned char*) 0x7C;
-volatile unsigned char* pADCSRB   = (unsigned char*) 0x7B;
-volatile unsigned char* pADCSRA   = (unsigned char*) 0x7A;
-volatile unsigned int*  pADC_DATA = (unsigned int*) 0x78;
-
-// GPIO Pointers
-volatile unsigned char* pDDRB  = (unsigned char*) 0x24;
-volatile unsigned char* pPORTB = (unsigned char*) 0x25;
-volatile unsigned char* pDDRH  = (unsigned char*) 0x101;
-volatile unsigned char* pPORTH = (unsigned char*) 0x102;
-volatile unsigned char* pDDRE  =
-
-unsigned long lastLCDUpdate = 0;
-extern LiquidCrystal lcd;
-
-// DHT object
-DHT dht(PIN_DHT11, DHTTYPE);
-
-// Prototypes
-void initIdleStateHardware();
-void handleIdleState();
-void handleErrorState();
-void handleRunningState();
-
-bool isStopPressed();
-bool isClearPressed();
-bool isWaterLow();
-float readTempC_stub();
-float readHumidity_stub();
-void printString(const char* str);
-void uartLog(const char* msg);
+volatile unsigned char* _ADMUX    = (unsigned char*) 0x7C;
+volatile unsigned char* _ADCSRB   = (unsigned char*) 0x7B;
+volatile unsigned char* _ADCSRA   = (unsigned char*) 0x7A;
+volatile unsigned int*  _ADC_DATA = (unsigned int*) 0x78;
 
 enum SystemState {
     STATE_DISABLED,
@@ -102,33 +75,99 @@ enum SystemState {
     STATE_RUNNING
 };
 
+// DHT object
+DHT dht(PIN_DHT11, DHTTYPE);
+
+// Prototypes
+void handleIdleState();
+void handleErrorState();
+void handleRunningState();
+
+bool isStopPressed();
+bool isResetPressed();
+bool isWaterLow();
+float readTempC();
+float readHumidity();
+void uartLog(const char* msg);
+void ISR_startButton();
+
+// Functions Declarations
+void U0Init(int baud);
+unsigned char U0KbHit();
+unsigned char U0getChar();
+void U0putChar(unsigned char ch);
+
+// Main Program
+
+LiquidCrystal lcd(PIN_DIS_RS, PIN_DIS_EN, PIN_DIS_D4, PIN_DIS_D5, PIN_DIS_D6, PIN_DIS_D7);
+unsigned long lastSensorPoll = 0;
+Stepper stepper(STEPPER_STEPS, PIN_STEPPER_IN1, PIN_STEPPER_IN3, PIN_STEPPER_IN2, PIN_STEPPER_IN4);
 volatile SystemState currentState = STATE_DISABLED;
 volatile bool startPressed = false;
+int stepperPrev = 0;
+
+void setup(){
+    U0Init(9600);
+    adcInit();
+    lcd.begin(16, 2);
+    dht.begin();
+
+    DDRH |= (1 << 4); // BLUE
+    DDRH |= (1 << 5); // GREEN
+    DDRH |= (1 << 6); // YELLOW
+    DDRB |= (1 << 4); // RED
+    DDRB |= (1 << 6); // FAN
+
+    DDRE &= ~(1 << 4);
+    PORTE |= (1 << 4); // START button
+    
+    attachInterrupt(digitalPinToInterrupt(2), ISR_startButton, FALLING);
+
+    DDRE &= ~(1 << 5);
+    PORTE |= (1 << 5); // STOP button
+
+    DDRG &= ~(1 << 5);
+    PORTG |= (1 << 5); // RESET button
+
+    PORTH &= ~(1 << 4); // BLUE OFF
+    PORTH &= ~(1 << 5); // GREEN OFF
+    PORTH &= ~(1 << 6); // YELLOW OFF
+    PORTB &= ~(1 << 4); // RED OFF
+    PORTB &= ~(1 << 6); // FAN OFF
+
+    stepper.setSpeed(26);
+    stepperPrev = adcRead(PIN_POTENTIOMETER);
+
+}
+
+void loop(){
+    switch (currentState) {
+        case STATE_DISABLED: handleDisabledState(); break;
+        case STATE_IDLE:     handleIdleState();     break;
+        case STATE_ERROR:    handleErrorState();    break;
+        case STATE_RUNNING:  handleRunningState();  break;
+    }
+
+    int potValue = adcRead(PIN_POTENTIOMETER);
+    int stepVal = potValue - stepperPrev;
+    if(stepVal != 0) stepper.step(stepVal);
+    stepperPrev = potValue;
+}
 
 void ISR_startButton() {
     startPressed = true; // ISR start button
 }
 
-void initDisabledStateHardware() {
-
-    // Yellow LED = PIN_LED_YELLOW = D9 = PH6
-    DDRH |= (1 << 6);    // Sets PH6 as OUTPUT
-    PORTH |= (1 << 6);   // LED ON at startup (DISABLED)
-
-    DDRE &= ~(1 << 4);   // PE4 input
-    PORTE |= (1 << 4);   // Enable pull-up
-
-    attachInterrupt(digitalPinToInterrupt(2), ISR_startButton, FALLING);
-}
+// State Handlers
 
 void handleDisabledState() {
     static bool firstEntry = true;
 
     if (firstEntry) {
         // YELLOW ON, all others OFF
-        PORTH |=  (1 << 6);                        // Yellow ON
-        PORTH &= ~((1 << 4) | (1 << 5));           // Blue & Green OFF
-        PORTB &= ~(1 << 4);                        // Red OFF
+        PORTH |=  (1 << 6);  // YELLOW ON
+        PORTH &= ~((1 << 4) | (1 << 5)); // BLUE & GREEN OFF
+        PORTB &= ~(1 << 4);  // RED OFF 
 
         // Fan OFF
         PORTB &= ~(1 << 6);
@@ -136,50 +175,52 @@ void handleDisabledState() {
         // LCD message
         lcd.clear();
         lcd.setCursor(0, 0);
+        lcd.print("SYSTEM");
+        lcd.setCursor(0, 1);
         lcd.print("DISABLED");
+
+        uartLog("[STATE] ENTER DISABLED");
 
         firstEntry = false;
     }
 
     // Only action in disabled state: start button
     if (startPressed) {
-      startPressed = false;
-
-      // Turns off yellow LED when leaving disabled
-      PORTH &= ~(1 << 6);
-
-      lastLCDUpdate = 0;        // <<< ADD THIS LINE (forces LCD update)
+      lastSensorPoll = 0;        // <<< ADD THIS LINE (forces LCD update)
 
       firstEntry = true;        // allow re-init next time
       currentState = STATE_IDLE;
+
+      startPressed = false;
     }
-
-}
-
-
-void initIdleStateHardware() {
-  // LEDs: Blue PH4(D7), Green PH5(D8), Yellow PH6(D9), Red PB4(D10)
-  DDRH |= (1 << 4) | (1 << 5) | (1 << 6);
-  DDRB |= (1 << 4);
-
-  // Fan: PB6 (D12) output
-  DDRB |= (1 << 6);
-
-  // Stop button: PE5 (D3) input with pull-up
-  DDRE &= ~(1 << 5);
-  PORTE |= (1 << 5);
-
-  // Clear button: PG5 (D4) input with pull-up
-  DDRG &= ~(1 << 5);
-  PORTG |= (1 << 5);
 }
 
 bool isStopPressed() {
-  return ((PINE & (1 << 5)) == 0);
+  bool isPressed = (PINE & (1 << 5)) == 0;
+
+  if(isPressed){
+    do {
+      isPressed = (PINE & (1 << 5)) == 0;
+      delay(50);
+    } while(isPressed);
+
+    return true;
+  }
+  return false;
 }
 
-bool isClearPressed() {
-  return ((PING & (1 << 5)) == 0);
+bool isResetPressed() {
+  bool isPressed = (PING & (1 << 5)) == 0;
+
+  if(isPressed){
+    do {
+      isPressed = (PING & (1 << 5)) == 0;
+      delay(50);
+    } while(isPressed);
+
+    return true;
+  }
+  return false;
 }
 
 bool isWaterLow() {
@@ -188,19 +229,31 @@ bool isWaterLow() {
 }
 
 // Replaced stub logic with real DHT11 reads
-float readTempC_stub() {
+float readTempC() {
   float t = dht.readTemperature();
-  return isnan(t) ? -100.0 : t;
+  if (isnan(t)) {
+    uartLog("[WARN] DHT temp read failed");
+    return -999.0f;
+  }
+  return t;
 }
 
-float readHumidity_stub() {
+float readHumidity() {
   float h = dht.readHumidity();
-  return isnan(h) ? -1.0 : h;
+  if (isnan(h)) {
+    uartLog("[WARN] DHT humidity read failed");
+    return -1.0f;
+  }
+  return h;
 }
 
 void uartLog(const char* msg) {
-  printString(msg);
-  printString("\n");
+  int i = 0;
+  while(msg[i] != '\0'){
+      U0putChar(msg[i]);
+      i++;
+  }
+  U0putChar('\n');
 }
 
 void handleIdleState() {
@@ -208,13 +261,9 @@ void handleIdleState() {
   unsigned long now = millis();
 
   if (firstEntry) {
-    initIdleStateHardware();
-    uartLog("[STATE] ENTER IDLE");
-
-    // GREEN ON, others OFF
+    // GREEN ON, all others OFF
     PORTH |=  (1 << 5);
-    PORTH &= ~(1 << 4);
-    PORTH &= ~(1 << 6);
+    PORTH &= ~((1 << 4) | (1 << 6));
     PORTB &= ~(1 << 4);
 
     // Fan OFF
@@ -224,15 +273,15 @@ void handleIdleState() {
     lcd.setCursor(0, 0);
     lcd.print("IDLE");
 
-    lastLCDUpdate = now;
+    uartLog("[STATE] ENTER IDLE");
+    lastSensorPoll = 0;
     firstEntry = false;
-  } 
-
+  }
 
   // STOP if detects stop button
   if (isStopPressed()) {
     uartLog("[EVENT] STOP -> DISABLED");
-    PORTB &= ~(1 << 6);     // Fan OFF
+    PORTB &= ~(1 << 6);   // Fan OFF
     firstEntry = true;
     currentState = STATE_DISABLED;
     return;
@@ -241,37 +290,35 @@ void handleIdleState() {
   // Water level monitored continuously
   if (isWaterLow()) {
     uartLog("[EVENT] WATER LOW -> ERROR");
-    PORTB &= ~(1 << 6);     // Fan OFF
+    PORTB &= ~(1 << 6); // Fan OFF
     firstEntry = true;
     currentState = STATE_ERROR;
     return;
   }
 
-  // -------- CONTROL LOGIC (NOT tied to LCD timing) --------
-  float t_now = readTempC_stub();
-  if (t_now > TEMP_HIGH_THRESHOLD_C) {
-    uartLog("[EVENT] TEMP HIGH -> RUNNING");
-    firstEntry = true;
-    currentState = STATE_RUNNING;
-    return;
-  }
-
   // -------- LCD DISPLAY (timed separately) --------
-  if (now - lastLCDUpdate >= LCD_UPDATE_INTERVAL) {
-    float h = readHumidity_stub();
+  if (now - lastSensorPoll >= LCD_UPDATE_INTERVAL) {
+    float t_now = readTempC();
+    if (t_now > TEMP_HIGH_THRESHOLD_C) {
+      uartLog("[EVENT] TEMP HIGH -> RUNNING");
+      firstEntry = true;
+      currentState = STATE_RUNNING;
+      return;
+    }
+    float h = readHumidity();
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("T:");
+    lcd.print("T: ");
     lcd.print(t_now, 1);
     lcd.print("C");
 
     lcd.setCursor(0, 1);
-    lcd.print("H:");
+    lcd.print("H: ");
     lcd.print(h, 0);
     lcd.print("%");
 
-    lastLCDUpdate = now;
+    lastSensorPoll = now;
   }
 }
 
@@ -282,15 +329,15 @@ void handleRunningState() {
 
   if (firstEntry) {
     // BLUE ON, all others OFF
-    PORTH |=  (1 << 4);                        // Blue ON
-    PORTH &= ~((1 << 5) | (1 << 6));           // Green, Yellow OFF
-    PORTB &= ~(1 << 4);                        // Red OFF
+    PORTH |=  (1 << 4);
+    PORTH &= ~((1 << 5) | (1 << 6));
+    PORTB &= ~(1 << 4);
 
     // Fan ON
-    PORTB |=  (1 << 6);
+    PORTB |= (1 << 6);
 
     // Force LCD to update immediately on entry
-    lastLCDUpdate = 0;
+    lastSensorPoll = 0;
 
     firstEntry = false;
   }
@@ -303,18 +350,23 @@ void handleRunningState() {
     return;
   }
 
-  // Transition to IDLE as soon as temperature drops below threshold
-  float t_now = readTempC_stub();
-  if (t_now <= TEMP_HIGH_THRESHOLD_C) {
-    PORTB &= ~(1 << 6); // Fan OFF
-    firstEntry = true;
-    currentState = STATE_IDLE;
+  if(isStopPressed()) {
+    uartLog("[EVENT] STOP -> DISABLED");
+    PORTB &= ~(1 << 6);   // Fan OFF
+    currentState = STATE_DISABLED;
     return;
   }
 
   // LCD shows Temperature + Water reading (timed)
-  if (now - lastLCDUpdate >= LCD_UPDATE_INTERVAL) {
-    unsigned int w = adcRead(PIN_WATER_SENSOR);   // raw ADC 0-1023
+  if (now - lastSensorPoll >= LCD_UPDATE_INTERVAL) {
+    float t_now = readTempC();
+    if (t_now <= TEMP_HIGH_THRESHOLD_C) {
+      PORTB &= ~(1 << 6); // Fan OFF
+      firstEntry = true;
+      currentState = STATE_IDLE;
+      return;
+    }
+    float h = readHumidity();
 
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -323,10 +375,11 @@ void handleRunningState() {
     lcd.print("C");
 
     lcd.setCursor(0, 1);
-    lcd.print("W:");
-    lcd.print(w);
+    lcd.print("H: ");
+    lcd.print(h, 0);
+    lcd.print("%");
 
-    lastLCDUpdate = now;
+    lastSensorPoll = now;
   }
 }
 
@@ -335,8 +388,11 @@ void handleErrorState() {
   static bool firstEntry = true;
 
   if (firstEntry) {
-    PORTH &= ~((1 << 4) | (1 << 5) | (1 << 6));
-    PORTB |=  (1 << 4);
+    PORTB |=  (1 << 4); // RED ON
+    PORTH &= ~((1 << 4) | (1 << 5)); // BLUE, GREEN OFF
+    PORTH &= ~(1 << 6); // YELLOW OFF
+
+    // Fan OFF
     PORTB &= ~(1 << 6);
 
     lcd.clear();
@@ -348,151 +404,38 @@ void handleErrorState() {
     firstEntry = false;
   }
 
-  if (isClearPressed() && !isWaterLow()) {
+  if (isResetPressed() && !isWaterLow()) {
     firstEntry = true;
     currentState = STATE_IDLE;
   }
 }
 
-
-// Functions Declarations
-void U0Init(int baud);
-unsigned char U0KbHit();
-unsigned char U0getChar();
-void U0putChar(unsigned char ch);
-void printString(const char* str);
-
-// Main Program
-
-LiquidCrystal lcd(PIN_DIS_RS, PIN_DIS_EN, PIN_DIS_D4, PIN_DIS_D5, PIN_DIS_D6, PIN_DIS_D7);
-Stepper stepper(STEPPER_STEPS, PIN_STEPPER_IN1, PIN_STEPPER_IN3, PIN_STEPPER_IN2, PIN_STEPPER_IN4);
-
-int stepperPrev = 0;
-
-void setup(){
-    initDisabledStateHardware();
-    U0Init(9600);
-    adcInit();
-    lcd.begin(16, 2);
-    dht.begin();
-    stepper.setSpeed(26);
-    stepperPrev = adcRead(PIN_POTENTIOMETER);
-
-    printString("--- Swap Cooler Final Project ---\n");
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.println("Final Project");
-    lcd.println("Swap Cooler");
-    delay(2000);
-}
-
-void loop(){
-    switch (currentState) {
-        case STATE_DISABLED: handleDisabledState(); return;
-        case STATE_IDLE:     handleIdleState();     return;
-        case STATE_ERROR:    handleErrorState();    return;
-        case STATE_RUNNING:  handleRunningState();  return;
-    }
-
-    int potValue = adcRead(PIN_POTENTIOMETER);
-    int stepVal = potValue - stepperPrev;
-    if(stepVal != 0) stepper.step(stepVal);
-    stepperPrev = potValue;
-}
-
-// Helpers for GPIO
-
-void getDDRPORTPIN(char* pin, volatile unsigned char** ddr, volatile unsigned char** port, volatile unsigned char** pinReg){
-    switch(*(pin + 1)){
-        case 'B':
-            *ddr = pDDRB;
-            *port = pPORTB;
-            break;
-        case 'E':
-            *ddr = pDDRE;
-            *port = pPORTE;
-            *pinReg = pPINE;
-            break;
-        case 'G':
-            *ddr = pDDRG;
-            *port = pPORTG;
-            *pinReg = pPING;
-            break;
-        case 'H':
-            *ddr = pDDRH;
-            *port = pPORTH;
-            break;
-        default:
-            return;
-    }
-}
-
-void setPinMode(char* pin, char mode){
-    volatile unsigned char* ddr;
-    volatile unsigned char* port;
-    getDDRandPORT(pin, &ddr, &port);
-    
-    unsigned char pinNum = *(pin + 2) - '0';
-
-    if(mode == 'o'){ // output
-        *ddr |= (1 << pinNum);
-    }else if(mode == 'i'){ // input
-        *ddr &= ~(1 << pinNum);
-    }else if(mode == 'ip'){ // input with pull-up
-        *ddr &= ~(1 << pinNum);
-        *port |= (1 << pinNum);
-    }
-}
-
-void setPinValue(char* pin, bool value){
-    volatile unsigned char* port;
-
-    switch(*(pin + 1)){
-        case 'B':
-            port = pPORTB;
-            break;
-        case 'H':
-            port = pPORTH;
-            break;
-        default:
-            return;
-    }
-
-    unsigned char pinNum = *(pin + 2) - '0';
-
-    if(value == true){
-        *port |= (1 << pinNum);
-    }else{
-        *port &= ~(1 << pinNum);
-    }
-}
-
 // ADC Functions
 
 void adcInit(){
-  *pADCSRA |= 0b10000000;
-  *pADCSRA &= 0b11011111;
-  *pADCSRA &= 0b11110111;
-  *pADCSRA &= 0b11111000;
-  *pADCSRA |= 0b00000111;
-  
-  *pADCSRB &= 0b11110111;
-  *pADCSRB &= 0b11111000;
-  
-  *pADMUX &= 0b01111111;
-  *pADMUX |= 0b01000000;
-  *pADMUX &= 0b11011111;
-  *pADMUX &= 0b11100000;
+  *_ADCSRA |= 0b10000000;
+  *_ADCSRA &= 0b11011111;
+  *_ADCSRA &= 0b11110111;
+  *_ADCSRA &= 0b11111000;
+  *_ADCSRA |= 0b00000111;
+
+  *_ADCSRB &= 0b11110111;
+  *_ADCSRB &= 0b11111000;
+
+  *_ADMUX &= 0b01111111;
+  *_ADMUX |= 0b01000000;
+  *_ADMUX &= 0b11011111;
+  *_ADMUX &= 0b11100000;
 }
 
 unsigned int adcRead(unsigned char channel){
-  *pADMUX &= 0b11100000;
-  *pADCSRB &= 0b11110111;
-  *pADMUX |= (channel & 0x1F);
-  *pADCSRA |= 0b01000000;
+  *_ADMUX &= 0b11100000;
+  *_ADCSRB &= 0b11110111;
+  *_ADMUX |= (channel & 0x1F);
+  *_ADCSRA |= 0b01000000;
 
-  while((*pADCSRA & 0x40) != 0);
-  return (*pADC_DATA & 0x03FF);
+  while((*_ADCSRA & 0x40) != 0);
+  return (*_ADC_DATA & 0x03FF);
 }
 
 // UART Functions
@@ -501,29 +444,21 @@ void U0Init(int baud){
     unsigned long FCPU = 16000000;
     unsigned int tbaud = (FCPU / 16 / baud - 1);
 
-    *pUCSR0A = 0x20;
-    *pUCSR0B = 0x18;
-    *pUCSR0C = 0x06;
-    *pUBRR0  = tbaud;
+    *_UCSR0A = 0x20;
+    *_UCSR0B = 0x18;
+    *_UCSR0C = 0x06;
+    *_UBRR0  = tbaud;
 }
 
 unsigned char U0KbHit(){
-    return *pUCSR0A & 0x80;
+    return *_UCSR0A & 0x80;
 }
 
 unsigned char U0getChar(){
-    return *pUDR0;
+    return *_UDR0;
 }
 
 void U0putChar(unsigned char ch){
-    while((*pUCSR0A & 0x20) == 0);
-    *pUDR0 = ch;
-}
-
-void printString(const char* str){
-    int i = 0;
-    while(str[i] != '\0'){
-        U0putChar(str[i]);
-        i++;
-    }
+    while((*_UCSR0A & 0x20) == 0);
+    *_UDR0 = ch;
 }
